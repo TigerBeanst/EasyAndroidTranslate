679b36d69cca96f7c0000baebe99f8e51e6099c2
hjthjthjt <i@jakting.com>
2020-08-04 21:14:21 +0800

    table support edit

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,19 @@
       <el-row id="input_form" :gutter="20">
         <el-col :span="12">
           <el-input
+              @click.native="open_source"
               type="textarea"
               :rows="2"
-              placeholder="Please paste the source strings.xml"
+              placeholder="Source strings.xml"
               v-model="source_form">
           </el-input>
         </el-col>
         <el-col :span="12">
           <el-input
+              @click.native="open_tran"
               type="textarea"
               :rows="2"
-              placeholder="Please paste the translate strings.xml"
+              placeholder="Translate strings.xml (optional)"
               v-model="translate_form">
           </el-input>
         </el-col>
@@ -29,6 +31,45 @@
         <el-button type="primary" @click="update_button">Update Translation</el-button>
       </div>
     </el-form>
+    <div id="table">
+      <el-table
+          ref="singleTable"
+          :data="strings"
+          highlight-current-row
+          @row-click="handleCurrentChange"
+          style="width: 100%">
+        <el-table-column
+            type="index"
+            width="50">
+        </el-table-column>
+        <el-table-column type="expand">
+          <template slot-scope="strings">
+            <el-form label-position="left" inline class="demo-table-expand">
+              <el-form-item label="Origin">
+                <span>{{ strings.row.origin }}</span>
+              </el-form-item>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column
+            property="key"
+            label="Key"
+            width="200">
+        </el-table-column>
+        <el-table-column
+            property="value"
+            label="Origin Value"
+            width="400">
+        </el-table-column>
+        <el-table-column
+            label="Translated Value">
+          <template scope="strings">
+            <el-input type="textarea" autosize v-model="strings.row.value" placeholder="Key"
+                      @change="handleEdit(strings.$index, strings.row)"></el-input>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -41,11 +82,14 @@ export default {
       activeIndex: '1',
       source_form: '',
       translate_form: '',
+      pre_source_form: '',
     }
   },
   methods: {
     start_button() {
-      //console.log(this.source_form)
+      if(this.source_form==""){
+        this.$message.error('Do not leave the "Source strings.XML" form empty');
+      }
       const regex = /<string name="(.*?)">(.*?)<\/string>/gm;
       let m;
       while ((m = regex.exec(this.source_form)) !== null) {
@@ -56,18 +100,38 @@ export default {
         }
         m.forEach((match, groupIndex) => {
           groupIndex;
-          if(n===0){
-            arr['origin']=match;
-          }else if(n===1){
-            arr['key']=match;
-          }else if(n===2){
-            arr['value']=match;
+          if (n === 0) {
+            arr['origin'] = match;
+          } else if (n === 1) {
+            arr['key'] = match;
+          } else if (n === 2) {
+            arr['value'] = match;
             this.strings.push(arr);
           }
           n++;
         });
       }
       console.log(this.strings)
+    },
+    open_source(){
+      const h = this.$createElement;
+      this.$notify({
+        title: 'Source strings.xml',
+        message: h('b', { style: 'color: teal'}, 'Paste strings.xml for the source here. This XML text should be standard formatted. Untranslatable text will not be displayed.')
+      });
+    },
+    open_tran(){
+      const h = this.$createElement;
+      this.$notify({
+        title: 'Translated strings.xml (optional)',
+        message: h('b', { style: 'color: teal'}, 'You can paste strings.xml that you\'ve translated before, and use it to check if the source strings.xml updated.')
+      });
+    },
+    handleCurrentChange(row, event, column) {
+      console.log(row, event, column, event.currentTarget)
+    },
+    handleEdit(index, row) {
+      console.log(index, row);
     },
     update_button() {
 
@@ -87,10 +151,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin: 0 auto;
-  width: 60%;
+  width: 80%;
 }
 
 #input_form, #input_button {
   margin-top: 30px;
+}
+
+.demo-table-expand {
+  font-size: 0;
+}
+
+.demo-table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+
+.demo-table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
 }
 </style>
